f8b1ded17a34243a4d8ea04677fc3e05508f5243
arielhasidim <ariel.hasidim@gmail.com>
2021-07-17 23:58:22 +0300

    created "copy" function (working around "disabling Ctrl+C")

--- data/custom.js
+++ data/custom.js
@@ -7,3 +7,28 @@ console.log("running custom script from extention");
   
     document.body.appendChild(style);
   })();
+
+  var ctrlDown = false,
+  ctrlKey = 17,
+  cmdKey = 91,
+  cKey = 67;
+
+  document.onkeydown = function(e) {
+      if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+      if (ctrlDown && (e.code == 'KeyC')) {
+
+        var selectedText = window.getSelection().toString();
+        
+        navigator.clipboard.writeText(selectedText).then(
+          function () {
+            console.log('success')
+          },
+          function () {
+            console.log('failure')
+          }
+      );
+      }
+  }
+  document.onkeyup = function(e) {
+      if (e.code == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+  };
