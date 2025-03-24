998f70b64b3c43cdf56e831ee2cb4f77c217c5b8
Ariel <23483971+arielhasidim@users.noreply.github.com>
2025-03-24 16:10:21 +0200

    no message

--- data/custom.js
+++ data/custom.js
@@ -1,34 +1,46 @@
 console.log("running custom script from extention");
-(function() {
-    'use strict';
-  
-    const style = document.createElement('style');
-    style.innerHTML = '*{ user-select: auto !important; }';
-  
-    document.body.appendChild(style);
-  })();
+(function () {
+  'use strict';
 
-  let ctrlDown = false,
+  const style = document.createElement('style');
+  style.innerHTML = '*{ user-select: auto !important; }';
+
+  document.body.appendChild(style);
+})();
+
+let ctrlDown = false,
   ctrlKey = 17,
   cmdKey = 91,
   cKey = 67;
 
-  document.onkeydown = function(e) {
-      if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
-      if (ctrlDown && (e.code == 'KeyC')) {
+let selectedText
 
-        let selectedText = window.getSelection().toString();
-        
-        navigator.clipboard.writeText(selectedText).then(
-          function () {
-            console.log('copy success')
-          },
-          function () {
-            console.log('copy failure. maybe update chrome?')
-          }
-      );
+// Overwrite what is being copied to the clipboard.
+document.addEventListener('copy', function(e) {
+  // e.clipboardData is initially empty, but we can set it to the
+  // data that we want copied onto the clipboard.
+  e.clipboardData.setData('text/plain', selectedText);
+
+  // This is necessary to prevent the current document selection from
+  // being written to the clipboard.
+  e.preventDefault();
+});
+
+document.onkeydown = function (e) {
+  selectedText = window.getSelection().toString();
+  if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+  if (ctrlDown && (e.code == 'KeyC')) {
+
+    navigator.clipboard.writeText(selectedText).then(
+      function () {
+        console.log('copy success: ' + selectedText)
+      },
+      function () {
+        console.log('copy failure. maybe update chrome?')
       }
+    );
   }
-  document.onkeyup = function(e) {
-      if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
-  };
+}
+document.onkeyup = function (e) {
+  if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+};
